3a909c394bdc136091eac481054866c6e6eb1983
a1eena59 <aleenawilson14@gmail.com>
2025-06-28 15:42:42 +0530

    Updated

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import "../css/Navbar.css"; // Assuming you have a CSS file for styling the NavB
 function NavBar() {
     return <nav className="navbar"> 
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/">Movieque</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
